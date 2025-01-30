24370efaec45dce3a8b742289f4a2c94d8548727
yuys13 <yuys13@users.noreply.github.com>
2025-01-30 18:14:05 +0900

    test: refactor

--- test/logger_spec.lua
+++ test/logger_spec.lua
@@ -1,73 +1,67 @@
----@diagnostic disable: param-type-mismatch, undefined-field
-local logger = require 'collama.logger'
-
+local assert = require 'luassert'
 local spy = require 'luassert.spy'
 
+local notify = spy.new(function() end)
+
 describe('default error level is INFO so', function()
-  after_each(function()
-    logger.setup(nil)
+  package.loaded['collama.logger'] = nil
+  local logger = require 'collama.logger'
+  ---@diagnostic disable-next-line: param-type-mismatch
+  logger.setup(notify)
+
+  before_each(function()
+    notify:clear()
   end)
 
   it('error call notify', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.error 'message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('warn call notify', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.warn 'message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('info call notify', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.info 'message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('debug do not call notify', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.debug 'message'
-    assert.spy(notify).was_not_called()
+    assert.spy(notify).was.called(0)
   end)
 end)
 
-describe('error function', function()
+describe('error()', function()
+  local logger = require 'collama.logger'
+  ---@diagnostic disable-next-line: param-type-mismatch
+  logger.setup(notify)
+
+  before_each(function()
+    notify:clear()
+  end)
+
   after_each(function()
     logger.set_minimum_level(vim.log.levels.INFO)
   end)
 
   it('do not call notify when error level is OFF', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.OFF)
 
     logger.error 'error message'
-    assert.spy(notify).was_not_called()
+    assert.spy(notify).was.called(0)
   end)
 
   it('call notify when error level is ERROR', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.ERROR)
 
     logger.error 'error message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('call notify with error level', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.error 'error message'
     assert.spy(notify).was.called_with('error message', vim.log.levels.ERROR, {
       title = 'collama',
@@ -77,33 +71,34 @@ describe('error function', function()
   end)
 end)
 
-describe('warn function', function()
+describe('warn()', function()
+  local logger = require 'collama.logger'
+  ---@diagnostic disable-next-line: param-type-mismatch
+  logger.setup(notify)
+
+  before_each(function()
+    notify:clear()
+  end)
+
   after_each(function()
     logger.set_minimum_level(vim.log.levels.INFO)
   end)
 
   it('do not call notify when error level is ERROR', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.ERROR)
 
     logger.warn 'warn message'
-    assert.spy(notify).was_not_called()
+    assert.spy(notify).was.called(0)
   end)
 
   it('call notify when error level is WARN', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.WARN)
 
     logger.warn 'warn message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('call notify with error level', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.warn 'warn message'
     assert.spy(notify).was.called_with('warn message', vim.log.levels.WARN, {
       title = 'collama',
@@ -113,33 +108,34 @@ describe('warn function', function()
   end)
 end)
 
-describe('info function', function()
+describe('info()', function()
+  local logger = require 'collama.logger'
+  ---@diagnostic disable-next-line: param-type-mismatch
+  logger.setup(notify)
+
+  before_each(function()
+    notify:clear()
+  end)
+
   after_each(function()
     logger.set_minimum_level(vim.log.levels.INFO)
   end)
 
   it('do not call notify when error level is WARN', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.WARN)
 
     logger.info 'info message'
-    assert.spy(notify).was_not_called()
+    assert.spy(notify).was.called(0)
   end)
 
   it('call notify when error level is INFO', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.INFO)
 
     logger.info 'info message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('call notify with error level', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
-
     logger.info 'info message'
     assert.spy(notify).was.called_with('info message', vim.log.levels.INFO, {
       title = 'collama',
@@ -149,32 +145,34 @@ describe('info function', function()
   end)
 end)
 
-describe('debug function', function()
+describe('debug()', function()
+  local logger = require 'collama.logger'
+  ---@diagnostic disable-next-line: param-type-mismatch
+  logger.setup(notify)
+
+  before_each(function()
+    notify:clear()
+  end)
+
   after_each(function()
     logger.set_minimum_level(vim.log.levels.INFO)
   end)
 
   it('do not call notify when error level is INFO', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.INFO)
 
     logger.debug 'debug message'
-    assert.spy(notify).was_not_called()
+    assert.spy(notify).was.called(0)
   end)
 
   it('call notify when error level is DEBUG', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.DEBUG)
 
     logger.debug 'debug message'
-    assert.spy(notify).was.called()
+    assert.spy(notify).was.called(1)
   end)
 
   it('call notify with error level', function()
-    local notify = spy.new(function() end)
-    logger.setup(notify)
     logger.set_minimum_level(vim.log.levels.DEBUG)
 
     logger.debug 'debug message'
